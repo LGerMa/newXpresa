f303e5ed8ace325cb3cc4f7063e4a660fcad3232
LGerMa <lgmm.93@gmail.com>
2016-02-27 19:55:18 -0600

    Home Responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,57 @@
-var gulp = require('gulp');
-var webserver = require('gulp-webserver');
-var nib = require('nib');
-var stylus = require('gulp-stylus');
-var minifyCss = require('gulp-minify-css');
-var jade = require('gulp-jade');
+var gulp = require('gulp'); // sirve para llamar la dependencia de gulp (GULP es un automatizador de tareas)
+var webserver = require('gulp-webserver'); // sirve para llamar la dependencia de GULP-SERVER y montar un servidor con gulp
+var nib = require ('nib');// sirve para llamar un plugin NIB que lo utilizamos para agregar prefijos a propiedades CSS para la compatibilidad en todos los navegadores
+var stylus = require ('gulp-stylus');//sirve para llamar al plugin stylus y poder crear codigo CSS optimizado y con mejores utilidades
+var minifyCSS = require ('gulp-minify-css');
+var jade = require ('gulp-jade');
+
 
 var config = {
 	styles: {
 		watch: './src/styles/*.styl',
-		output: './build/css/'
-		},
-	html:{
+		output: './build/css'
+	},
+	html: {
 		watch: './src/*.jade',
 		output: './build'
 	}
 }
 
-gulp.task('server',function(){
+
+gulp.task('server', function () {
 	gulp.src('./build')
 	.pipe(webserver({
 		host: '0.0.0.0',
 		port: '8080',
-		livereload: true
-		}));
-	});
+		livereload:true
+	}));
+});
 
-gulp.task('render-html',function(){
-	gulp.src(config.html.watch)
-	.pipe(jade({
-		pretty:true
-		}))
-	.pipe(gulp.dest(config.html.output));
-	});
+gulp.task('build:html', function(){
+gulp.src(config.html.watch)
+.pipe(jade({
+	pretty:true
+}))
+.pipe(gulp.dest(config.html.output));
+});
 
-gulp.task('render-css',function(){
+gulp.task('build:css', function (){
 	gulp.src(config.styles.watch)
 	.pipe(stylus({
 		use:nib(),
 		'include css':true
-		}))
-	.pipe(minifyCss())
+	}))
+	.pipe(minifyCSS())
 	.pipe(gulp.dest(config.styles.output));
-	});
+});
 
-gulp.task('watch',function(){
-	gulp.watch(config.html.watch, ['render-html']);
-	gulp.watch(config.styles.watch, ['render-css']);
-	});
 
-gulp.task('default',['server','watch']);
+gulp.task('watch', function(){
+	gulp.watch(config.styles.watch, ['build:css']);
+	gulp.watch(config.html.watch, ['build:html']);
+});
+
+
+gulp.task('default', ['server', 'watch']);
+
+
